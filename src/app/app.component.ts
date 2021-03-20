127ee24c916c9afbc7f8c7b968f62d8110078ba8
Csaba-Gyozo Kudor <csaba-gyozo.kudor@ubisoft.com>
2021-03-20 11:43:22 +0200

    added counter as regular component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-wc-simple';
+    title = 'ng-wc-simple';
+
+    @Input() counter = 0;
+
+    @Output() countChanged = new EventEmitter<number>();
+
+    constructor() { }
+
+    ngOnInit(): void { }
+
+    inc() {
+        this.counter++;
+        this.countChanged.next(this.counter);
+    }
+
+    dec() {
+        this.counter--;
+        this.countChanged.next(this.counter);
+    }
 }
